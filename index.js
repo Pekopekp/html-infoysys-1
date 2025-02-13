91f5a46a03c92a4c9ca7f3cf948fdca2254ccad9
Pekopeko <sanaba771@gmail.com>
2025-02-13 21:34:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function showLoading() {
     if (loadingDiv) {
       loadingDiv.style.display = 'flex'; // Make the loading div visible
       setTimeout(() => {
-        window.location.href = './main1/val1.html'; // Redirect to the next page
+        window.location.href = './main1/index.html'; // Redirect to the next page
       }, 3000); // 3-second delay
     } else {
       console.error('Loading div not found!');
